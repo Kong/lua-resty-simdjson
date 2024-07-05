3366443af23643dadf9213cc6537a4e497fe5dc8
chronolaw <chrono_cpp@me.com>
2024-07-05 14:19:51 +0800

    function-try

--- simdjson_ffi.cpp
+++ simdjson_ffi.cpp
@@ -67,147 +67,151 @@ static bool simdjson_process_value(simdjson_ffi_state &state, T&& value) {
 }
 
 
-extern "C" {
-    simdjson_ffi_state *simdjson_ffi_state_new() {
-        return new(std::nothrow) simdjson_ffi_state();
-    }
-
-    simdjson_ffi_op_t *simdjson_ffi_state_get_ops(simdjson_ffi_state *state) {
-        return state->ops;
-    }
-
-    void simdjson_ffi_state_free(simdjson_ffi_state *state) {
-        delete state;
-    }
+extern "C"
+simdjson_ffi_state *simdjson_ffi_state_new() {
+    return new(std::nothrow) simdjson_ffi_state();
+}
 
 
-    int simdjson_ffi_parse(simdjson_ffi_state *state, const char *json, size_t len, const char **errmsg) {
-        try {
-            state->json = padded_string(json, len);
+extern "C"
+simdjson_ffi_op_t *simdjson_ffi_state_get_ops(simdjson_ffi_state *state) {
+    return state->ops;
+}
 
-            state->document = state->parser.iterate(state->json);
-            state->ops_n = 0;
 
-            simdjson_process_value(*state, state->document);
+extern "C"
+void simdjson_ffi_state_free(simdjson_ffi_state *state) {
+    delete state;
+}
 
-        } catch (simdjson_error &e) {
-            *errmsg = e.what();
 
-            return SIMDJSON_FFI_ERROR;
+extern "C"
+int simdjson_ffi_parse(simdjson_ffi_state *state,
+    const char *json, size_t len, const char **errmsg) try {
+
+    state->json = padded_string(json, len);
+
+    state->document = state->parser.iterate(state->json);
+    state->ops_n = 0;
+
+    simdjson_process_value(*state, state->document);
+
+    return state->ops_n;
+
+} catch (simdjson_error &e) {
+    *errmsg = e.what();
+
+    return SIMDJSON_FFI_ERROR;
+}
+
+
+extern "C"
+int simdjson_ffi_next(simdjson_ffi_state *state, const char **errmsg) try {
+    state->ops_n = 0;
+
+    while (!state->frames.empty()) {
+
+        if (state->ops_n >= SIMDJSON_FFI_BATCH_SIZE - 1) {
+            // -1 for key value pair which requires 2 ops
+            return state->ops_n;
         }
 
-        return state->ops_n;
-    }
+        auto &frame = state->frames.top();
 
-    int simdjson_ffi_next(simdjson_ffi_state *state, const char **errmsg) {
-        try {
-            state->ops_n = 0;
-
-            while (!state->frames.empty()) {
-
-                if (state->ops_n >= SIMDJSON_FFI_BATCH_SIZE - 1) {
-                    // -1 for key value pair which requires 2 ops
-                    return state->ops_n;
+        switch (frame.state) {
+            case simdjson_ffi_resume_state::array: {
+                if (frame.processing) {
+                    ++frame.it.array.current;
+                    frame.processing = false;
                 }
 
-                auto &frame = state->frames.top();
+                // resume array iteration
+                for (auto it = frame.it.array.current; it != frame.it.array.end; ++it) {
+                    ondemand::value value = *it;
 
-                switch (frame.state) {
-                    case simdjson_ffi_resume_state::array: {
-                        if (frame.processing) {
-                            ++frame.it.array.current;
-                            frame.processing = false;
-                        }
-
-                        // resume array iteration
-                        for (auto it = frame.it.array.current; it != frame.it.array.end; ++it) {
-                            ondemand::value value = *it;
-
-                            if (simdjson_process_value(*state, value)) {
-                                // save state, go deeper
-                                frame.it.array.current = it;
-                                frame.processing = true;
-
-                                break;
-                            }
-
-                            if (state->ops_n >= SIMDJSON_FFI_BATCH_SIZE) {
-                                // array can use the last of the slots, no need to
-                                // reserve two slots like object below
-                                frame.it.array.current = it;
-                                frame.processing = true;
-
-                                return state->ops_n;
-                            }
-                        }
-
-                        if (!frame.processing) {
-                            state->frames.pop();
-
-                            state->ops[state->ops_n++].opcode = SIMDJSON_FFI_OPCODE_RETURN;
-                        }
+                    if (simdjson_process_value(*state, value)) {
+                        // save state, go deeper
+                        frame.it.array.current = it;
+                        frame.processing = true;
 
                         break;
                     }
 
-                    case simdjson_ffi_resume_state::object: {
-                        // resume object iteration
+                    if (state->ops_n >= SIMDJSON_FFI_BATCH_SIZE) {
+                        // array can use the last of the slots, no need to
+                        // reserve two slots like object below
+                        frame.it.array.current = it;
+                        frame.processing = true;
 
-                        if (frame.processing) {
-                            ++frame.it.object.current;
-                            frame.processing = false;
-                        }
-
-                        for (auto it = frame.it.object.current; it != frame.it.object.end; ++it) {
-                            auto field = *it;
-                            std::string_view key = field.unescaped_key();
-
-                            state->ops[state->ops_n].opcode = SIMDJSON_FFI_OPCODE_STRING;
-                            state->ops[state->ops_n].str = key.data();
-                            state->ops[state->ops_n++].size = key.size();
-
-                            // this can not overflow, because we checked to make sure
-                            // ops has at least 2 empty slots above
-
-                            if (simdjson_process_value(*state, field.value())) {
-                                // save state, go deeper
-                                frame.it.object.current = it;
-                                frame.processing = true;
-
-                                break;
-                            }
-
-                            if (state->ops_n >= SIMDJSON_FFI_BATCH_SIZE - 1) {
-                                frame.it.object.current = it;
-                                frame.processing = true;
-
-                                return state->ops_n;
-                            }
-                        }
-
-                        if (!frame.processing) {
-                            state->frames.pop();
-
-                            state->ops[state->ops_n++].opcode = SIMDJSON_FFI_OPCODE_RETURN;
-                        }
-
-                        break;
+                        return state->ops_n;
                     }
-
-                    default:
-                        SIMDJSON_UNREACHABLE();
                 }
+
+                if (!frame.processing) {
+                    state->frames.pop();
+
+                    state->ops[state->ops_n++].opcode = SIMDJSON_FFI_OPCODE_RETURN;
+                }
+
+                break;
             }
 
-        } catch (simdjson_error &e) {
-            *errmsg = e.what();
+            case simdjson_ffi_resume_state::object: {
+                // resume object iteration
 
-            return SIMDJSON_FFI_ERROR;
+                if (frame.processing) {
+                    ++frame.it.object.current;
+                    frame.processing = false;
+                }
+
+                for (auto it = frame.it.object.current; it != frame.it.object.end; ++it) {
+                    auto field = *it;
+                    std::string_view key = field.unescaped_key();
+
+                    state->ops[state->ops_n].opcode = SIMDJSON_FFI_OPCODE_STRING;
+                    state->ops[state->ops_n].str = key.data();
+                    state->ops[state->ops_n++].size = key.size();
+
+                    // this can not overflow, because we checked to make sure
+                    // ops has at least 2 empty slots above
+
+                    if (simdjson_process_value(*state, field.value())) {
+                        // save state, go deeper
+                        frame.it.object.current = it;
+                        frame.processing = true;
+
+                        break;
+                    }
+
+                    if (state->ops_n >= SIMDJSON_FFI_BATCH_SIZE - 1) {
+                        frame.it.object.current = it;
+                        frame.processing = true;
+
+                        return state->ops_n;
+                    }
+                }
+
+                if (!frame.processing) {
+                    state->frames.pop();
+
+                    state->ops[state->ops_n++].opcode = SIMDJSON_FFI_OPCODE_RETURN;
+                }
+
+                break;
+            }
+
+            default:
+                SIMDJSON_UNREACHABLE();
         }
-
-        // we are done! clean up the tmp string to save memory
-        state->json = padded_string();
-
-        return state->ops_n;
     }
+
+    // we are done! clean up the tmp string to save memory
+    state->json = padded_string();
+
+    return state->ops_n;
+
+} catch (simdjson_error &e) {
+    *errmsg = e.what();
+
+    return SIMDJSON_FFI_ERROR;
 }
